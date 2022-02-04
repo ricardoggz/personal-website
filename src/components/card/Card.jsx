bfd3754f8dc7a1e01bcc9ec69494af38f325726c
ricardoggz <ricardo_ggzicm@outlook.com>
2022-02-04 12:08:22 -0600

    hero modificado

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Typed from "react-typed";
 import { CardWrapper, ImageContainer, CardText } from "./card.elements";
 export const Card = () => {
   return (
     <CardWrapper>
       <CardText>
         <span className="title">Ricardo Guevara</span>
-        <span className="subtitle">Desarrollador Frontend</span>
+        <span className="subtitle">
+          <Typed
+            strings={["Desarrollador Frontend"]}
+            typeSpeed={80}
+            backSpeed={80}
+            loop
+          />
+        </span>
       </CardText>
     </CardWrapper>
   );
