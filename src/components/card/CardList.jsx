31e094f3b0c66aea0c53535680597fe8db9cdc66
ricardoggz <ricardo_ggzicm@outlook.com>
2022-02-10 13:01:19 -0600

    hero mejorado para deploy

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -5,12 +5,27 @@ import { FiMail } from "react-icons/fi";
 
 export const CardList = () => {
   return (
-      <ListContainer>
-          <ul>
-              <li><a href="#"><BsGithub /></a></li>
-              <li><a href="#"><BsLinkedin /></a></li>
-              <li><a href="#"><FiMail /></a></li>
-          </ul>
-      </ListContainer>
+    <ListContainer>
+      <ul>
+        <li>
+          <a href="https://github.com/ricardoggz" target={"_blank"}>
+            <BsGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/ricardoguevarag-a33657216/"
+            target={"_blank"} 
+          >
+            <BsLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:ricardo_ggzicm@outlook.com">
+            <FiMail />
+          </a>
+        </li>
+      </ul>
+    </ListContainer>
   );
 };
