a1984b19df6eea41e0d440fcce2785461af4e029
ricardoggz <ricardo_ggzicm@outlook.com>
2022-03-08 18:03:16 -0600

    haremos rediseño de todo

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { NavBar } from "../components/navBar/NavBar";
-import { Hero } from "../components/hero/Hero";
-import { About } from "../components/about/About";
-import { Projects } from "../components/projects/Projects";
-import { Footer } from "../components/footer/Footer";
 
 export const App = () => {
   return (
     <React.Fragment>
-      <NavBar />
-      <Hero />
-      <About />
-      <Projects />
-      <Footer />
+     <h1>Holaa</h1>
     </React.Fragment>
   );
 };
