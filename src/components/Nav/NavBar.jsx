41331575b7cf4bd0bea7b65c6a5f9646b78afb3d
ricardoggz <ricardo_ggzicm@outlook.com>
2022-05-19 16:26:41 -0500

    NavBar para mobile mejorado

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,15 +1,24 @@
-import React from "react";
-import { NavWrapper, NavList, Logo } from "./elements";
+import React, { useState } from "react";
+import { FaBars, FaTimesCircle } from "react-icons/fa";
+import { NavWrapper, NavList, Logo, NavItems } from "./elements";
 
 const NavBar = () => {
+  const [ menu, setMenu] = useState(false);
+  const showMenu = () => {
+    setMenu(!menu);
+  }
   return (
     <NavWrapper>
       <NavList className="flex-container">
         <Logo><a href="#">RGDEV</a></Logo>
-        <ul>
-          <li><a href="#about">Sobre mí</a></li>
-          <li><a href="#portfolio">Portafolio</a></li>
-        </ul>
+        <button onClick={showMenu}>
+          {menu ? <FaTimesCircle /> : <FaBars />}
+        </button>
+        <NavItems open={menu}>
+          <li><a href="#" onClick={showMenu}>Inicio</a></li>
+          <li><a href="#about" onClick={showMenu}>Sobre mí</a></li>
+          <li><a href="#portfolio" onClick={showMenu}>Portafolio</a></li>
+        </NavItems>
       </NavList>
     </NavWrapper>
   );
--- src/components/Nav/elements.js
+++ src/components/Nav/elements.js
@@ -5,37 +5,65 @@ const NavWrapper = styled.header`
   background: var(--color-blue-primary);
   color: var(--color-light-primary);
   padding: 1rem 0;
+
+  @media (min-width: 240px) and (max-width: 750px) {
+    position: fixed;
+  }
 `;
 const NavList = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  ul li {
-    list-style: none;
-    display: inline;
+  button {
+    display: none;
   }
 
-  ul li a {
+  @media (min-width: 240px) and (max-width: 750px) {
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: none;
+      color: var(--color-ligth-primary);
+      font-size: 1.5rem;
+      border: none;
+    }
+  }
+`;
+
+const NavItems = styled.ul`
+  display: flex;
+  gap: 1rem;
+
+  li {
+    list-style: none;
+  }
+
+  li a {
     text-decoration: none;
-    padding: 1rem;
     color: var(--color-light-primary);
   }
 
-  ul li a:hover {
-    background: var(--color-light-primary);
-    color: var(--color-blue-primary);
-    transition: all 300ms ease;
-    border-radius: 8px;
-  }
+  @media (min-width: 240px) and (max-width: 780px) {
+    background: var(--color-blue-primary);
+    gap: 2rem;
+    left: 0;
+    right: 0;
+    position: fixed;
+    top: 3.5rem;
+    width: 100%;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: 500ms all ease-in-out;
+    transform: translateX(${({ open }) => (open ? "0" : "-100%")});
 
-  @media (min-width:240px) and (max-width:720px) {
-    flex-direction:column;
-    justify-content:center;
-    ul li a:hover {
-
-      background:none;
-      color:var(--color-light-primary);
-      
+    li {
+      width:90%;
+      border-bottom:1px solid var(--color-gray);
+      text-align:center;
+      padding: 1rem 0;
     }
   }
 `;
@@ -43,13 +71,9 @@ const Logo = styled.span`
   font-size: 1.2rem;
   font-weight: 700;
   a {
-    text-decoration:none;
-    color:var(--color-light-primary);
-  }
-
-  @media (min-width:240px) and (max-width:720px) {
-    padding: 1rem 0;
+    text-decoration: none;
+    color: var(--color-light-primary);
   }
 `;
 
-export { NavWrapper, NavList, Logo };
+export { NavWrapper, NavList, NavItems, Logo };
